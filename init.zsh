a4a3de0a8d1e49b8bf2b25adbd833a361746e92a
xged <xged90@gmail.com>
2019-01-30 06:26:09 +0200

    - xged-init

--- init.zsh
+++ init.zsh
@@ -1,31 +1,24 @@
 xged-init-pkgs() {
-    sudo pacman -S --noconfirm --needed yaourt
-    yaourt -S --noconfirm --needed bluez-utils google-chrome emacs adobe-source-code-pro-fonts tree aspell-en xorg-xbacklight
+    sudo pacman -S --noconfirm --needed yay
+    yay -S --noconfirm --needed bluez-utils google-chrome emacs adobe-source-code-pro-fonts tree aspell-en xorg-xbacklight xdotool xsel
+}
+xged-init-main() {
+    sudo mount $1 /mnt
+    cp -r /mnt/home/xged/src $HOME
+    cp -r /mnt/home/xged/.emacs.d $HOME
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+    echo 'export ZDOTDIR=$HOME/src/config/zsh' >$HOME/.zshenv
+    # relog
+}
+xged-init-xfce() {
+    # set Settings_Manager/Window_Manager/Keyboard/Cycle_Windows to <F12>
+    # set Settings_Manager/Keyboard/Application_Shortcuts: < sh -c 'xdotool type "$(xsel)"' > to <C-SPC>
 }
 xged-init-fs() {
     rm -rf Music Pictures Public Templates Videos
-}
-xged-init-zsh() {
-    echo 'export ZDOTDIR=$HOME/src/config/zsh' >$HOME/.zshenv  # this dir
-    chsh -s $(which zsh)
-}
-xged-init-spacemacs() {
-    git clone https://github.com/syl20bnr/spacemacs --branch develop --single-branch $HOME/.emacs.d
 }
 xged-init-git() {
     git config --global user.name "xged"
     git config --global user.email "xged90@gmail.com"
     git config --global rebase.autoStash true
-}
-xged-init() {
-    xged-init-pkgs
-    xged-init-fs
-    # Copy old ~/src
-    xged-init-zsh #! needs relogging
-    # xged-init-xfce:
-    #     set Settings_Manager/Window_Manager/Keyboard/Cycle_Windows to <F12>
-    #     set Settings_Manager/Power_Manager/Display/Put_to_sleep_after&Switch_off_after to Never
-    #     set <Terminal-window>/Edit/Preferences/Appearance/Background to None(use solid color)
-    xged-init-spacemacs #| copy old ~/.emacs.d
-    xged-init-git
 }
