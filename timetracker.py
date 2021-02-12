d42a068c0bae8435091e88aa280583be5bbc96e0
xged <xged90@gmail.com>
2021-02-12 22:05:04 +0200

    17

--- timetracker.py
+++ timetracker.py
@@ -1,21 +1,21 @@
-from datetime import datetime, timedelta
-import pickle
 import os
+import pickle
 from copy import deepcopy as cp
+from datetime import datetime, timedelta
 
 fp = '/home/xged/src/data/.timetracker.pickle'
-datainit = {"tracking_start": None, "day_start": None, "day_end": 23, "timestamps": [], "work_hours": {}}
+datainit = {"tracking_start": None, "day_start": None, "day_end": 23, "work_hours": {}, "timestamps": []}
 
 def main():
     currenttime = datetime.now()
     year, week, day = datetime.isocalendar(currenttime)
     with open(fp, "rb+") as f:
         data = pickle.load(f)
-        if data["work_hours"].setdefault(year, {}).setdefault(week, [0, 0, 0, 0, 0, 0, 0])[day-1] == 0:
-            data["day_start"] = cp(currenttime) - timedelta(hours=0.5)
         if data["tracking_start"] is None:
             print("Started Tracking..")
             data["tracking_start"] = currenttime
+            if data["work_hours"].setdefault(year, {}).setdefault(week, [0, 0, 0, 0, 0, 0, 0])[day-1] == 0:
+                data["day_start"] = cp(currenttime) - timedelta(hours=0.5)
         else:
             currentwork = (currenttime - data["tracking_start"]).total_seconds()/3600
             data["work_hours"][year][week][day-1] += currentwork
@@ -44,21 +44,24 @@ def write(data):
     with open(fp, "wb") as f:
         pickle.dump(data, f)
 
-def merge(d):
-    def _merge(source, destination):
-        for key, value in source.items():
-            if isinstance(value, dict):
-                node = destination.setdefault(key, {})
-                _merge(value, node)
-            else:
-                destination[key] = value
-        return destination
-    with open(fp, "rb+") as f:
-        data = pickle.load(f)
-        _merge(d, data)
-        f.seek(0)
-        pickle.dump(data, f)
-    print(data)
+def _merge(source, destination):
+    for key, value in source.items():
+        if isinstance(value, dict):
+            node = destination.setdefault(key, {})
+            _merge(value, node)
+        else:
+            destination[key] = value
+    return destination
+
+def update_hours(minutes, day=None, week=None, year=None):
+    currentyear, currentweek, currentday = datetime.isocalendar(datetime.now())
+    if day is None: day = currentday
+    if week is None: day = currentweek
+    if year is None: day = currentyear
+    data = read()
+    data["work_hours"][year][week][day] += minutes/60
+    write()
+    print(data["work_hours"])
 
 def week_avgs():
     with open(fp, "rb") as f:
