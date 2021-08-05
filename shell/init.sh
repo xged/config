0244ac189b049d82c7580cb507458ba941840eca
xged <xged90@gmail.com>
2021-08-05 10:44:05 +0300

    BIG

--- shell/init.sh
+++ shell/init.sh
@@ -1,20 +1,18 @@
-xged-init-sda() {  # xged-init-sda /dev/sda3
     sudo pacman -S --noconfirm --needed yay
-    yay -S --noconfirm --needed bluez-utils google-chrome emacs adobe-source-code-pro-fonts tree aspell-en xorg-xbacklight xsel xvkbd redshift gnirehtet timew
+    yay -S --noconfirm --needed google-chrome-dev emacs adobe-source-code-pro-fonts tree aspell-en xorg-xbacklight xsel xvkbd xrandr gnirehtet timew scrcpy imagewriter piper-git
 
-    export ZDOTDIR=$HOME/src/config/shell
-    echo 'export ZDOTDIR=$HOME/src/config/shell' >$HOME/.zshenv
+    export ZDOTDIR=home/xged/src/config/shell
+    echo 'export ZDOTDIR=home/xged/src/config/shell' >/home/xged/.zshenv
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-    export HISTSIZE=1000000
 
-    sudo mount $1 /mnt
-    cp -r /mnt/$HOME/src /mnt/$HOME/.emacs.d /mnt/$HOME/.config /mnt/$HOME/.zsh_history /mnt/$HOME/.xinitrc $HOME
+    # cp -r /mnt/home/xged/src /mnt/home/xged/.emacs.d /mnt/home/xged/.zsh_history .
     cp -r /mnt/etc/default/grub /etc/default
+    update-grub
 
-    rm -rf $HOME/Music $HOME/Pictures $HOME/Public $HOME/Templates $HOME/Videos $HOME/Documents $HOME/Games
+    # rm -rf /home/xged/Music /home/xged/Pictures /home/xged/Public /home/xged/Templates /home/xged/Videos /home/xged/Documents /home/xged/Games
     sudo nmcli radio wifi off  # disable wireless
     git config --global user.name "xged"
     git config --global user.email "xged90@gmail.com"
     git config --global rebase.autoStash true
-    #! relog
-}
+    export HISTSIZE=1000000
+    # restart
