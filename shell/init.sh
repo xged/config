9c0cf5720d1242263f102ced47df7c7186a6e66f
Julius Gedvilas <xged90@gmail.com>
2021-08-06 21:22:01 +0300

    RUSH

--- shell/init.sh
+++ shell/init.sh
@@ -1,14 +1,20 @@
     sudo pacman -S --noconfirm --needed yay
-    yay -S --noconfirm --needed base-devel archlinux-keyring google-chrome-dev emacs adobe-source-code-pro-fonts tree aspell-en xorg-xbacklight xsel xvkbd xorg-xrandr gnirehtet timew scrcpy imagewriter piper-git skypeforlinux-stable-bin notepadqq
+    yay -S --noconfirm --needed base-devel archlinux-keyring google-chrome emacs adobe-source-code-pro-fonts tree aspell-en xorg-xbacklight xsel xvkbd xorg-xrandr gnirehtet timew scrcpy imagewriter piper-git skypeforlinux-stable-bin notepadqq
 
     export ZDOTDIR=home/xged/src/config/shell
     echo 'export ZDOTDIR=home/xged/src/config/shell' >/home/xged/.zshenv
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+    
+    mkdir src; cd src
+    git clone https://github.com/xged/config
+    git clone https://github.com/xged/misc
+    git clone https://github.com/xged/python-scripts
+    git clone https://github.com/xged/private    
+    
+    sudo cp /home/xged/src/config/grub /etc/default; sudo update-grub
 
-    cp -r /mnt/home/xged/src /mnt/home/xged/.emacs.d /mnt/home/xged/.zsh_history .
-    cp -r /mnt/etc/default/grub /etc/default
-    update-grub
-
+    cp -r /mnt/home/xged/.emacs.d /mnt/home/xged/.zsh_history .
+  
     rm -rf /home/xged/Music /home/xged/Pictures /home/xged/Public /home/xged/Templates /home/xged/Videos /home/xged/Documents /home/xged/Games
     sudo nmcli radio wifi off  # disable wireless
     git config --global user.name "xged"
